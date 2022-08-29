0ca4a31204c9ada0127c206f7d32428d872352e1
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-29 05:40:58 -0500

    Update script

--- rails/rails-create.sh
+++ rails/rails-create.sh
@@ -13,16 +13,23 @@ cd "$1"
 # add ".env" and "node_modules/" to .gitignore file
 sed -i 's/\.env\.local/\.env\n&/' .gitignore
 echo "
+.env
 node_modules/" >> .gitignore
 
-# configure database
-touch .env
+# create environment variables in .env
 echo "# The database credentials
 DATABASE_HOST=localhost
 DATABASE_USER=postgres
-DATABASE_PASSWORD=m4st3rk3y" >> .env
-cp .env .env.example
+DATABASE_PASSWORD=m4st3rk3y
+DEVISE_JWT_SECRET_KEY=[THIS IS A SECRET. DO NOT TELL ANYBODY.]" > .env
 
+echo "# Set environment variables. [Remove square brackets]
+DATABASE_HOST=[HOST]
+DATABASE_USER=[USER]
+DATABASE_PASSWORD=[PASSWORD]
+DEVISE_JWT_SECRET_KEY=[SECRET]" > .env-example
+
+# set database credentials in config/database.yml
 sed -i 's/pool:.*/&\n  host: <%= ENV["DATABASE_HOST"] %>\n  username: <%= ENV["DATABASE_USER"] %>\n  password: <%= ENV["DATABASE_PASSWORD"] %>/' config/database.yml
 
 # download MIT.md && README.md
@@ -36,26 +43,84 @@ wget -O .github/pull_request_template.md https://raw.githubusercontent.com/rbano
 wget -O .github/workflows/linters.yml https://raw.githubusercontent.com/microverseinc/linters-config/master/ror/.github/workflows/linters.yml
 wget -O .rubocop.yml https://raw.githubusercontent.com/microverseinc/linters-config/master/ror/.rubocop.yml
 wget -O .stylelintrc.json https://raw.githubusercontent.com/microverseinc/linters-config/master/ror/.stylelintrc.json
+# delete line
+sed  -i '/.*IgnoredMethods/d' ./.rubocop.yml
 
-# Update Gemfile, install
-  # - dotenv-rails,
-  # - rails-controller-testing
-  # - rspec-rails
-  # - rubocop
-sed -i 's/gem "rails", "~.*/&\n\ngem "dotenv-rails", require: "dotenv\/rails-now"/;t;s/gem "debug".*/&\n  gem "rails-controller-testing"\n  gem "rspec-rails", ">= 5.0", "< 6.0"/;t;s/gem \"spring\"/&\n\n  # linters\n  gem "rubocop", ">= 1\.0", "< 2\.0"/' ./Gemfile
+
+# Update Gemfile, install core gems,
+sed -i 's/gem "rails", "~.*/&\n\n# Load environment variables\ngem "dotenv-rails", require: "dotenv\/rails-now"/' ./Gemfile
+sed -i 's/gem "rails", "~.*/&\n\ngem "ffi"/' ./Gemfile
+
+# Update Gemfile, install gems in development group,
+sed -i 's/group :development do/&\n  #  Preview email\n  gem "letter_opener"/' ./Gemfile
+sed -i 's/gem "letter_opener"/&\n\n  #  Linter Ruby files\n  gem "rubocop", ">= 1\.0", "< 2\.0"/' ./Gemfile
+
+# Update Gemfile, install gems in development/test group,
+sed -i 's/group :development, :test do/&\n  # Help to kill N+1 queries and unused eager loading\n  gem "bullet"/' ./Gemfile
+
+echo 'group :test do
+  # Testing framework
+  gem "rails-controller-testing"
+  gem "rspec-rails"
+  gem "simplecov", require: false
+
+  # Integration testing tools
+  gem "capybara"
+  gem "webdrivers"
+end' >> Gemfile
+
+# Update Gemfile, install gems for Authentication && Authorization
+sed -i 's/gem "dotenv-rails".*/&\n\n# Authentication\ngem "devise", ">= 4.0", "< 5.0"/' ./Gemfile
+sed -i 's/gem "devise".*/&\n\n# Authorization\ngem "cancancan", ">= 3.0", "< 4.0"/' ./Gemfile
+sed -i 's/gem "devise".*/&\n\n# JWT authentication for devise\ngem "devise-jwt"/' ./Gemfile
+sed -i 's/gem "dotenv-rails".*/&\n\n# Make Rack-based apps CORS compatible\ngem "rack-cors"/' ./Gemfile
+
+# Update Gemfile, install gems for API documentation
+sed -i 's/group :development, :test do/gem "rswag-api"\ngem "rswag-ui"\n\n&\n  gem "rswag-specs"\n/' ./Gemfile
 
 bundle install
-#npm install -S stylelint@13.x stylelint-scss@3.x stylelint-config-standard@21.x stylelint-csstree-validator@1.x
+
+# Setup Bullet
+echo "y" | rails g bullet:install
+
+# Configure letter_opener
+config="  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }\n  config.action_mailer.delivery_method = :letter_opener\n  config.action_mailer.perform_deliveries = true"
+sed -i 's/config.action_mailer.perform_caching = false/&\n'"$config"'/' config/environments/development.rb
 
 # Setup Rspec
 rails generate rspec:install
 
+mkdir ./spec/support
+sed -i 's/# Dir\[Rails/Dir[Rails/' ./spec/rails_helper.rb
+  # Configure devise
+rails generate devise:install
+sed -i 's/<%= yield %>/<p class="notice"><%= notice %><\/p>\n    <p class="alert"><%= alert %><\/p>\n    &/' app/views/layouts/application.html.erb
+echo "
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Warden::Test::Helpers
+" > ./spec/support/devise.rb
+  # Generate devise views
+rails g devise:views
+find ./app/views/devise/ -type f -exec sed -i 's/{ method: :post }/{ "data-turbo": "false", method: :post }/' {} \;
+find ./app/views/devise/ -type f -exec sed -i 's/{ method: :put }/{ "data-turbo": "false", method: :put }/' {} \;
+
+# Configure capybara
+echo "require 'capybara/rspec'
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless
+  end
+end
+" > ./spec/support/capybara.rb
+
 #create database
 rails db:create
 
-# Run linters
-rubocop -A
-npx stylelint **/*.{css,scss} --fix
+# Install and run linters
+npm install -S stylelint@13.x stylelint-scss@3.x stylelint-config-standard@21.x stylelint-csstree-validator@1.x
+sed -i 's/"dependencies": {/"scripts": {\n    "linters": "rubocop -A -o log\/rubocop.log \&\& stylelint **\/*.{css,scss} --fix"\n  },\n  &/' ./package.json
+npm run linters
 
 if [ -z "$2" ]; then
   git remote add origin git@github.com:rbanos-mv/"$1".git
@@ -72,4 +137,14 @@ if [ -z "$2" ]; then
   git add .
   git commit -m "project setup"
   git push -u origin dev
+else
+  git add .
+  git commit -m "project setup"
 fi
+
+echo "
+*****************************************************************************
+* IMPORTANT:
+*
+*****************************************************************************
+"
